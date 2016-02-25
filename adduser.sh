551bc86b9f358931b4ec2209f56d92ef6ed4a3a2
Lais <l_d91@txstate.edu>
2016-02-24 23:57:42 -0600

    AddUser

---
+++ adduser.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DBFILE="/home/lais/contactlist.txt"
+addUser()
+{
+   echo "Enter a first name"
+   checkInput fname
+   echo "Enter a last name"
+   checkInput lname
+   echo "Enter an address"
+   checkInput  address
+   echo "Enter the zip code"
+   checkInput zip
+   echo "Enter Home Phone"
+   checkInput phone
+   echo "Enter Work Phone"
+   checkInput phone2
+   echo "Enter email"
+   checkInput email
+   echo "Enter age"
+   checkInput age
+   echo "$fname:$lname:$address:$zip:$phone:$phone2:$email:$age" >> "$DBFILE"
+}
+
+checkInput()
+{
+  local _resultvar=$1
+  read var
+  if [[ $var == *[:]* ]]
+  then
+     echo "Your entry contains a colon which is not permited, please try again"
+     checkInput var
+  fi
+  eval $_resultvar="'$var'"
+}
+addUser
