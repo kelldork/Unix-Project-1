4ab497a961f865c61b8820e610adacefc0c9b4ca
klk2 <klk2@txstate.edu>
2016-02-29 02:59:40 -0600

    Update findrecord.sh

--- findrecord.sh
+++ findrecord.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-echo "Enter a first OR last name to"
-echo "search (use correct capitalization):"
-read KEY
 DBFILE="contactlist.txt"
 
-if grep -q $KEY $DBFILE; then
-  echo " "
-  echo "Records containing '$KEY':"
-  echo -e "$(grep $KEY $DBFILE | tr ":" " ")\n"
-else
-  echo "No records containing '$KEY'."
-  exit 0
-fi
+findRecord() {
+  echo "Enter a first OR last name to"
+  echo "search (use proper capitalization):"
+  read KEY
+  
+  if grep -q $KEY $DBFILE; then
+    echo " "
+    echo "Records containing '$KEY':"
+    echo -e "$(grep $KEY $DBFILE | tr ":" " ")\n"
+  else
+    echo "No records containing '$KEY'."
+    exit 0
+  fi
+}
+findRecord
