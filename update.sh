b1c100680e0b61d22321dea40b205b490dc0732f
Sean Miller <scm84@txstate.edu>
2016-02-29 18:42:09 -0600

    cleaned up update and added name

--- update.sh
+++ update.sh
@@ -12,17 +12,6 @@ ContactFields()
 	echo "8: Age"; echo
 }
 
-# Prompt for which contact to change
-# Currently doesn't work if number entered is greater than number of contacts
-#while true; do
-#	bash display.sh
-#    read -p "Enter number of contact to update: " contact
-#    case $contact in
-#		[[:digit:]] ) break;;
-#        * ) bash display.sh; echo "Invalid; Please enter a number: ";;
-#	esac
-#done
-
 # Print contacts
 bash display.sh
 
@@ -42,31 +31,5 @@ sed -i "$contact d" contactlist.txt
 # Add new user
 echo "Please enter new information: "
 bash adduser.sh
-
-
-
-
-
-# Print field options
-# ContactFields
-
-# Prompt for which field to change
-#while true; do
-#	read -p "Enter field to update: " field
-#	case $field in
-#		[1-8] ) break;;
-#		* ) echo "Invalid; Please enter a number: "; ContactFields;;
-#	esac
-#done
-
-# Prompt for new information
-#echo "Enter new info (must not contain colon): "
-#while true; do
-#	read -p "Enter new info (must not contain colon): " info
-#	case $info in
-#		) break;;
-#		* ) echo "Invalid, contained colon. Please enter new info: ";;
-#	esac
-#done
 
 exit 0
