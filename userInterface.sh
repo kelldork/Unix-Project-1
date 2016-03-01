bf50c874af7656a9f340ac11a5a69aff1917aa73
Azaan Dhalla <amd166@txstate.edu>
2016-02-29 19:10:36 -0600

    optimized things

--- userInterface.sh
+++ userInterface.sh
@@ -1,58 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
 pause()
 {
-	echo -n "\nPress enter to continue..."
+	echo -n "Press enter to continue..."
 	read temp
 }
 
-userInterface()
-{
-	while true ; do
-		clear
-		echo 'Welcome to our contact database, please select one of the following menu options:'
-		echo '(a) Find a record'
-		echo '(b) Add a new record'
-		echo '(c) Update a record'
-		echo '(d) Remove a record'
-		echo '(e) Display all records'
-		echo '(f) Quit'
-		echo -n '> Selection is : '
-	
-		read input
-	
-		case $input in
-			[Aa]* ) clear
-					bash findrecord.sh
-					pause;; 
+while true ; do
+	clear
+	echo 'Welcome to our contact database, please select one of the following menu options:'
+	echo '(a) Find a record'
+	echo '(b) Add a new record'
+	echo '(c) Update a record'
+	echo '(d) Remove a record'
+	echo '(e) Display all records'
+	echo '(f) Quit'
+	echo -n '> Selection is : '
 
-			[Bb]* ) clear
-					bash adduser.sh
-					echo "\nNew record has been added to the database."
-					pause;;
+	read input
 
-			[Cc]* ) clear
-					;;
+	case $input in
+		[Aa]* )	clear
+				./findrecord.sh
+				pause;; 
 
-			[Dd]* ) clear
-					bash removeRecord.sh
-					echo "\nRecord has been removed from the database."
-					pause;;
+		[Bb]* )	clear
+				./adduser.sh
+				echo "New record has been added to the database."
+				pause;;
 
-			[Ee]* ) clear
-					./display.sh
-					pause;;
+		[Cc]* )	clear
+				./update.sh
+				;;
 
-			[Ff]* ) clear
-					echo "Good Bye!"
-					sleep 1
-					clear
-					return 0;;
+		[Dd]* )	clear
+				./removeRecord.sh
+				echo "Record has been removed from the database."
+				pause;;
 
-			* 	  ) clear
-					echo "Incorrect input!"
-			    	sleep 2;;
-		esac
-	done
-}
-userInterface
+		[Ee]* )	clear
+				./display.sh
+				pause;;
+
+		[Ff]* )	clear
+				echo "Good Bye!"
+				sleep 1
+				clear
+				exit 0;;
+
+		* 	  )	clear
+				echo "Incorrect input!"
+		    	sleep 2;;
+	esac
+done
